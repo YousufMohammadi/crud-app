0783bb5dc67b1f767c56183918c4c6409a8acf95
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-08-24 12:44:51 +0430

    some changes added

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,25 +1,49 @@
-import "./../styles/login.css"
+import "./../styles/login.css";
 import { useNavigate } from "react-router-dom";
+import { useGlobal } from "./zustand";
 export function Login() {
-   const navigate = useNavigate();
-    function handleLogin() {
-        navigate("home")
+  const navigate = useNavigate();
+  const users = useGlobal((state) => state.users);
+
+  function handleLogin(e) {
+    e.preventDefault();
+    let exist = false;
+    users.map((user) => {
+      if (
+        user.userName == e.target.userName.value &&
+        user.pass == e.target.password.value
+      ) {
+        exist = true;
+      }
+    });
+
+    if (exist) {
+      navigate("/Home");
+    } else {
+      alert("incorrect usernam or pawsowed");
     }
-return(
-
+  }
+  return (
     <>
-    <div className="login-container">
-    
-        <form  id="login-form">
-        <input type="text" placeholder="Enter your username" className="user-input"></input> 
-        <input type="password" placeholder="Enter your Password" className="user-pass"></input> 
-        <input type="submit" value="Login" className="btn-login" onClick={handleLogin}></input> 
+      <div className="login-container">
+        <form id="login-form" onSubmit={handleLogin} method="POST" action="#">
+          <input
+            type="text"
+            placeholder="Enter your username"
+            id="userName"
+            name="userName"
+            className="user-input"
+          ></input>
+          <input
+            type="password"
+            placeholder="Enter your Password"
+            id="password"
+            name="password"
+            className="user-pass"
+          ></input>
+          <input type="submit" value="Login" className="btn-login"></input>
         </form>
-
-    </div>
-    
+      </div>
     </>
-  
-)
+  );
 }
- 
